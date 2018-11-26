e7c9eb65ad2c7befa15648b5e320388866c8722e
Morten Bay Skov <mort79e5@student.eadania.dk>
2018-11-26 13:55:56 +0100

    77

--- LimboSoulsOfJudgement/LimboSoulsOfJudgement/Player.cs
+++ LimboSoulsOfJudgement/LimboSoulsOfJudgement/Player.cs
@@ -11,16 +11,11 @@ namespace LimboSoulsOfJudgement
 {
     public class Player : Character
     {
-        public Player() : base(3, 12, new Vector2(GameWorld.ScreenSize.Width / 2, GameWorld.ScreenSize.Height/2), "Melee2")
 
         /// <summary>
         /// Player constructor that sets player animation values, position and sprite name
         /// </summary>
-        /// <param name="frameCount"></param>
-        /// <param name="animationFPS"></param>
-        /// <param name="startPostion"></param>
-        /// <param name="spriteName"></param>
-        public Player(int frameCount, float animationFPS, Vector2 startPostion, string spriteName) : base(frameCount, animationFPS, startPostion, spriteName)
+        public Player() : base(3, 12, new Vector2(GameWorld.ScreenSize.Width / 2, GameWorld.ScreenSize.Height/2), "Melee2")
         {
             //Maximum amount of Player health
             maxHealth = 100;
@@ -59,7 +54,7 @@ namespace LimboSoulsOfJudgement
             if (Keyboard.GetState().IsKeyDown(Keys.D))
             {
                 facingRight = true;
-                position.Y += (float)(movementSpeed * gameTime.ElapsedGameTime.TotalSeconds);
+                position.X += (float)(movementSpeed * gameTime.ElapsedGameTime.TotalSeconds);
             }
 
             //Statement that checks if the Player is jumping, and handles Player jumpforce while in the air 
--- LimboSoulsOfJudgement/LimboSoulsOfJudgement/Soul.cs
+++ LimboSoulsOfJudgement/LimboSoulsOfJudgement/Soul.cs
@@ -8,8 +8,29 @@ namespace LimboSoulsOfJudgement
 {
     public class Soul : AnimatedGameObject
     {
+        /// <summary>
+        /// Field that sets the amount of Souls contained in the current Soul GameObject
+        /// </summary>
+        public int souls;
+
+        /// <summary>
+        /// Soul Constructor that sets Soul animation values, position and sprite name
+        /// </summary>
+        /// <param name="frameCount"></param>
+        /// <param name="animationFPS"></param>
+        /// <param name="startPostion"></param>
+        /// <param name="spriteName"></param>
         public Soul(int frameCount, float animationFPS, Vector2 startPostion, string spriteName) : base(frameCount, animationFPS, startPostion, spriteName)
         {
+        }
+
+        /// <summary>
+        /// Method that updates Soul game logic
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
         }
     }
 }
