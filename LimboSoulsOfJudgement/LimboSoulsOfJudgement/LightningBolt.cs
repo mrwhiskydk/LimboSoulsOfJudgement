30bbdc9bed81bfd79f8119e304c98863e5bfc89c
mrwhiskydk <sein@live.dk>
2018-12-06 12:47:50 +0100

    Ability system 0.1

--- LimboSoulsOfJudgement/LimboSoulsOfJudgement/LightningBolt.cs
+++ LimboSoulsOfJudgement/LimboSoulsOfJudgement/LightningBolt.cs
@@ -7,27 +7,56 @@ using Microsoft.Xna.Framework;
 
 namespace LimboSoulsOfJudgement
 {
-    class LightningBolt : Ability
+
+    public class LightningBolt : Ability
     {
-        private int speed = 1500;
-        public int damage = 25;
-        private Vector2 dir;
+        
 
-        public LightningBolt(Vector2 startPosition, Vector2 dir) : base(4, 100, startPosition, "Lightning")
+        public LightningBolt() : base(new Vector2(300, 300), "LightningBolt")
         {
-            this.dir = dir;
-
-            if (this.dir != Vector2.Zero)
-            {
-                this.dir.Normalize();
-            }
-
-            rotation = (float)Math.Atan2(dir.Y, dir.X);
+            cooldown = 1;
+            cooldownTimer = cooldown;
         }
 
-        public override void Update(GameTime gameTime)
+        public override void UseAbility()
         {
-            position += dir * speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            Vector2 playerPosition = GameWorld.player.Position;
+            Vector2 direction = new Vector2(GameWorld.mouse.Position.X, GameWorld.mouse.Position.Y) - playerPosition;
+
+            new LightningBoltAbility(playerPosition, direction);
+        }
+
+
+        class LightningBoltAbility : AnimatedGameObject
+        {
+            private int speed = 1000;
+            public int damage = 25;
+            private Vector2 dir;
+            private double timeAlive = 0;
+
+            public LightningBoltAbility(Vector2 startPosition, Vector2 dir) : base(4, 20, startPosition, "Lightning")
+            {
+                this.dir = dir;
+
+                if (this.dir != Vector2.Zero)
+                {
+                    this.dir.Normalize();
+                }
+
+                rotation = (float)Math.Atan2(dir.Y, dir.X);
+            }
+
+            public override void Update(GameTime gameTime)
+            {
+                position += dir * speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                base.Update(gameTime);
+
+                timeAlive += gameTime.ElapsedGameTime.TotalSeconds;
+                if (timeAlive >= 3)
+                {
+                    Destroy();
+                }
+            }
         }
     }
 }
