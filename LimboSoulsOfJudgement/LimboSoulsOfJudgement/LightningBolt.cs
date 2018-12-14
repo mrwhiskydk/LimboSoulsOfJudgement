f4a45145434a7061ea0499ab3bfb717d91b10f3b
Morten Bay Skov <mort79e5@student.eadania.dk>
2018-12-14 11:23:28 +0100

    Har rykket Overriden Draw Metode fra Ability over i Lightning Bolt,- CoolDown Draw Metoden ligger stadig i AbilityCooldown klassen som Mathias har oprettet. Har defor fjernet Draw Metoden fra selve Ability Class, da den nu bliver overridden i LightningBolt

--- LimboSoulsOfJudgement/LimboSoulsOfJudgement/LightningBolt.cs
+++ LimboSoulsOfJudgement/LimboSoulsOfJudgement/LightningBolt.cs
@@ -33,6 +33,23 @@ namespace LimboSoulsOfJudgement
             position = UIAbilityBar.abilitySlot1;
         }
 
+        /// <summary>
+        /// Overridden Draw Method that draws out the sprite of Lightning Bolt Ability, if purchased through BuyLightningBoltButton Class.
+        /// While not purchased, the sprite is still being drawn out, but it is 100% transparent.
+        /// </summary>
+        /// <param name="spriteBatch"></param>
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            if (GameWorld.buyLightningBoltButton.abilityPurchased)
+            {
+                spriteBatch.Draw(sprite, position, null, Color.White, rotation, new Vector2(sprite.Width * 0.5f, sprite.Height * 0.5f), 1f, SpriteEffects.None, 0.992f);
+            }
+            else
+            {
+                spriteBatch.Draw(sprite, position, null, Color.White * 0.0f, rotation, new Vector2(sprite.Width * 0.5f, sprite.Height * 0.5f), 1f, SpriteEffects.None, 0.992f);
+            }
+
+        }
 
         class LightningBoltAbility : AnimatedGameObject
         {
