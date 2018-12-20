16d34ff8e6cfb0303d0012a7a43c799537423643
Jonathan Herning Gitz-Johansen <jona176r@student.eadania.dk>
2018-12-20 14:54:05 +0100

    wd

--- LimboSoulsOfJudgement/LimboSoulsOfJudgement/Platform.cs
+++ LimboSoulsOfJudgement/LimboSoulsOfJudgement/Platform.cs
@@ -8,6 +8,11 @@ namespace LimboSoulsOfJudgement
 {
     public class Platform : GameObject
     {
+        /// <summary>
+        /// Constructer for the gameobject Platform
+        /// </summary>
+        /// <param name="startPosition">The starting position of the platform</param>
+        /// <param name="spriteName">The name of the sprite used for platforms</param>
         public Platform(Vector2 startPosition, string spriteName) : base(startPosition, spriteName)
         {
         }
