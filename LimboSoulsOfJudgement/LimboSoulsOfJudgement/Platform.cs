a89a0c9eedde129db8412a8c4df84f933ce5411c
Mathias <Mathias@LAPTOP-9FSN3NE0>
2018-12-20 14:52:33 +0100

    :(

--- LimboSoulsOfJudgement/LimboSoulsOfJudgement/Platform.cs
+++ LimboSoulsOfJudgement/LimboSoulsOfJudgement/Platform.cs
@@ -6,6 +6,9 @@ using Microsoft.Xna.Framework;
 
 namespace LimboSoulsOfJudgement
 {
+    /// <summary>
+    /// Public Class that represents the functionality and game logic of the Platform GameObject
+    /// </summary>
     public class Platform : GameObject
     {
         public Platform(Vector2 startPosition, string spriteName) : base(startPosition, spriteName)
