ca56d8cd3ebc5b1a37e990f58d5a210f5c8039be
Morten Bay Skov <mort79e5@student.eadania.dk>
2018-12-20 13:59:07 +0100

    777

--- LimboSoulsOfJudgement/LimboSoulsOfJudgement/Portal.cs
+++ LimboSoulsOfJudgement/LimboSoulsOfJudgement/Portal.cs
@@ -7,10 +7,13 @@ using Microsoft.Xna.Framework;
 
 namespace LimboSoulsOfJudgement
 {
+    /// <summary>
+    /// Public Class that represents the functionality and game logic of the Portal GameObject
+    /// </summary>
     public class Portal : AnimatedGameObject
     {
         /// <summary>
-        /// Portal's Constructor, that sets the default start position and sprite name
+        /// Portal's Constructor, that sets the default frame count, animation FPS(Frames Per Second) start position and sprite name
         /// </summary>
         /// <param name="startPostion">default position of the Portal GameObject</param>
         public Portal(Vector2 startPostion) : base(8, 8, startPostion, "Portal")
